c8f614795807ab80f91ab68ca617aafaf0ba7f01
averyreyna <avery.reyna16@gmail.com>
2021-09-13 11:26:59 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Avery Reyna
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // inputs 1-4 contains the inputted strings for the mad lib
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String inputNoun = input.nextLine();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Enter a verb: ");
+        String inputVerb = input2.nextLine();
+
+        Scanner input3 = new Scanner(System.in);
+        System.out.print("Enter and adjective: ");
+        String inputAdj = input3.nextLine();
+
+        Scanner input4 = new Scanner(System.in);
+        System.out.print("Enter and adverb: ");
+        String inputAdverb = input4.nextLine();
+
+        // used a formatting method for string substitution to avoid tons concatenation
+        System.out.println(String.format("Do you %s your %s %s %s? That's hilarious!", inputVerb, inputAdj, inputNoun, inputAdverb));
+    }
+}
